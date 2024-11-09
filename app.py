bf1178516fc1cb9df04bb307c708d5933d0e23e8
usman <usmankhanuuu52@gmail.com>
2024-11-09 12:49:35 +0500

    initial

---
+++ app.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return b - a
+
+
+def multi(a, b):
+    return a * b
+
+
+result1 = add(5, 19)
+
+
+result2 = sub(8, 6)
+
+
+result3 = multi(4, 7)
+
+print(result1)
+print(result2)
+print(result3)
